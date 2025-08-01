7de33dcdbd1f1b6e79d8d2aae5c5777dc01b8905
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 22:07:28 -0300

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,16 @@
+Implemente a função h definida recursivamente por: h(m, n) = m + 1, se n = 1; h(m, n) = n + 1, se m = 1, h(m, n) = h(m, n − 1) + h(m − 1, n), se m > 1, n > 1.
+
+
+def h(m, n):
+    
+    if n == 1:
+        return m + 1
+    elif m == 1:
+        return n + 1
+    else:
+        return h(m, n - 1) + h(m - 1, n)
+
+x1 = int(input(""))
+x2 = int(input(""))
+y = h(x1, x2)
+print(f"{y}")
